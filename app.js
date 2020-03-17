ee170163fb6cb07de832f53ec34cec557bbb234f
prashant.tomar <prashant.tomar@rapidops.com>
2020-03-17 18:26:10 +0530

    uploaded first time

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+
+app.listen(3000, function () {
+    console.log("helo");
+});
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/in.html");
+})
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb+srv://prashant_tomar:qwertyuiop23@clustermeg-dkebi.mongodb.net/test?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+var User = mongoose.model("User", nameSchema);
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post("/addname", (req, res) => {
+
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("item saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        });
+});
